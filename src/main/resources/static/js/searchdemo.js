97a5f65755514d23cd473d2c5f62cf27695afffd
YamadaChiaki <969522665@qq.com>
2018-07-11 20:39:43 +0800

    change by lichao

--- src/main/resources/static/js/searchdemo.js
+++ src/main/resources/static/js/searchdemo.js
@@ -36,17 +36,22 @@
 		searchContainer.classList.add('search--open');
 		inputSearch.focus();
 
-		//		var search = document.getElementById("main");
-		//		search.classList.add('main_hide2');
-		var search1 = document.getElementById("main_top");
-		search1.classList.add('main_top_hide');
-		var search2 = document.getElementById("main_medium");
-		search2.classList.add('main_medium_hide');
-		var search3 = document.getElementById("main_bottom");
-		search3.classList.add('main_bottom_hide');
-		
+		var search1 = document.getElementById('main');
+		search1.classList.add('main_hide');
 
-		var searchScene = document.getElementById("demo-2");
+		//		var search1 = document.getElementById('main_top');
+		//		search1.classList.add('main_top_hide');
+		//		var search2 = document.getElementById('main_medium');
+		//		search2.classList.add('main_medium_hide');
+		//		var search3 = document.getElementById('main_bottom');
+		//		search3.classList.add('main_bottom_hide');
+		var search4 = document.getElementById("loginScene");
+		search4.classList.add('loginScene_hide');
+		
+		var search5 = document.getElementById("search");
+		search5.classList.remove('search--open_hide');
+
+		var searchScene = document.getElementById('demo-2');
 		searchScene.style.zIndex = 1;
 		searchScene.style.opacity = 1;
 		searchScene.style.filter = blur(0);
@@ -58,15 +63,21 @@
 		inputSearch.blur();
 		inputSearch.value = '';
 
-		//		var search = document.getElementById("main");
-		//		search.classList.remove('main_hide2');
-		var search1 = document.getElementById("main_top");
-		search1.classList.remove('main_top_hide');
-		var search2 = document.getElementById("main_medium");
-		search2.classList.remove('main_medium_hide');
-		var search3 = document.getElementById("main_bottom");
-		search3.classList.remove('main_bottom_hide');
-		
+		//		var search1 = document.getElementById('main_top');
+		//		search1.classList.remove('main_top_hide');
+		//		var search2 = document.getElementById('main_medium');
+		//		search2.classList.remove('main_medium_hide');
+		//		var search3 = document.getElementById('main_bottom');
+		//		search3.classList.remove('main_bottom_hide');
+
+		var search1 = document.getElementById('main');
+		search1.classList.remove('main_hide');
+
+		var search4 = document.getElementById('loginScene');
+		search4.classList.remove('loginScene_hide');
+		var search5 = document.getElementById("search");
+		search5.classList.add('search--open_hide');
+
 	}
 
 	init();
